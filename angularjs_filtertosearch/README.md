9c245601662b9a3ab7c6f410745f3deef2043b91
zpeng <cfps91152@gmail.com>
2016-07-07 18:33:18 +0800

    add filter-to-search

---
+++ angularjs_filtertosearch/README.md
@@ -0,0 +1,4 @@
+#filter-to-search
+- - -
+查詢功能
+
---
+++ angularjs_filtertosearch/app_3.js
@@ -0,0 +1,25 @@
+angular.module('app', []).controller("MainController", function() {
+  this.people = [
+      {
+        name : "John Doe",
+        phone : "555-1111"
+      },
+      {
+        name : "Bill Smith",
+        phone : "555-2222"
+      },
+      {
+        name : "Mary Jones",
+        phone : "555-3333"
+      },
+      {
+        name : "Jane Doe",
+        phone : "555-1111"
+      },
+      {
+        name : "Ted Jones",
+        phone : "555-3333"
+      },
+  ];
+  
+});
